9a474f53070ad5bebba2e84fc7808e8342cfa359
Koushik Khan <koushikkhan38@gmail.com>
2025-02-10 10:17:59 +0530

    updated text for intro
    Co-authored-by: Akshay Agrawal <akshaykagrawal7@gmail.com>

--- polars/01_why_polars.py
+++ polars/01_why_polars.py
@@ -29,7 +29,7 @@ def _(mo):
 
         Polars' performance is due to a number of factors, including its implementation and rust and its ability to perform operations in a parallelized and vectorized manner. It supports a wide range of data types, advanced query optimizations, and seamless integration with other Python libraries, making it a versatile tool for data scientists, engineers, and analysts. Additionally, Polars provides a lazy API for deferred execution, allowing users to optimize their workflows by chaining operations and executing them in a single pass.
 
-        With its focus on speed, scalability, and ease of use, Polars is quickly becoming a go-to choice for data professionals looking to streamline their data processing pipelines and tackle large-scale data challenges. Whether you're analyzing gigabytes of data or performing real-time computations, Polars empowers you to work faster and smarter.
+        With its focus on speed, scalability, and ease of use, Polars is quickly becoming a go-to choice for data professionals looking to streamline their data processing pipelines and tackle large-scale data challenges.
         """
     )
     return
