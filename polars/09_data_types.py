6a372e0d1c05c6a2be5205e616d9ddaa0d3accf2
Debajyoti Debnath <debajyotid2@hotmail.com>
2025-04-02 13:39:29 -0400

    Add note on marimo's notebook inspection features

--- polars/09_data_types.py
+++ polars/09_data_types.py
@@ -113,6 +113,9 @@ def _(mo):
         ### Inspecting a dataframe
 
         Polars has various functions to explore the data in a dataframe. We will use the dataframe `data` defined above in our examples. Alongside we can also see a view of the dataframe rendered by `marimo` as the cells are executed.
+
+        ///note
+        We can also use `marimo`'s built in data-inpection elements/features such as  [`mo.ui.dataframe`](https://docs.marimo.io/api/inputs/dataframe/#marimo.ui.dataframe) & [`mo.ui.data_explorer`](https://docs.marimo.io/api/inputs/data_explorer/). For more check out our Polars tutorials at [`marimo learn`](https://marimo-team.github.io/learn/)!
         """
     )
     return
